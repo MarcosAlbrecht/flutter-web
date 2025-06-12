ce36ad844e4e49baaf71a24762efc1ec0fcb9880
Your Name <marcosralbrecht@gmail.com>
2025-06-11 23:04:55 -0300

    first commit

---
+++ lib/src/app_module.dart
@@ -0,0 +1,14 @@
+// Ensure Bind is imported
+import 'package:flutter_modular/flutter_modular.dart';
+
+import './modules/home/home_page.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {
+    r.child('/', child: (context) => HomePage());
+  }
+}
